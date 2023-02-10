bfb299408c6e2ed94a65a61d59b080158c302d95
Benny Reyes <benny.reyes@wizeline.com>
2023-02-10 15:33:36 -0600

    Initial Commit

---
+++ XibTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XibTest
+//
+//  Created by Benny Reyes on 10/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
